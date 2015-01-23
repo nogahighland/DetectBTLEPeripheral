ac008ffe29b3440f8d64a5b519b0aca095c8d96b
noga.highland@googlemail.com <noga.highland@googlemail.com>
2015-01-24 02:49:32 +0900

    Initial Commit

---
+++ DetectBTLEPeripheral/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DetectBTLEPeripheral
+//
+//  Created by noga.highland on 2015/01/24.
+//  Copyright (c) 2015年 nogahighland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DetectBTLEPeripheral/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DetectBTLEPeripheral
+//
+//  Created by noga.highland on 2015/01/24.
+//  Copyright (c) 2015年 nogahighland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
